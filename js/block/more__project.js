40ba614168a330ad50bc6e85614db1e783cb3047
roman <19roman23@gmail.com>
2020-02-19 23:07:36 +0200

    modified more project script

--- js/block/more__project.js
+++ js/block/more__project.js
@@ -4,6 +4,13 @@ jQuery(document).ready(function(){
   var blockItem = $(".portfolio__colum");
   blockItem.hide();
   var sliceCount = 3;
+  if($(window).width() <= 1024){
+    sliceCount = 2;
+  }else if($(window).width() <= 991){
+    sliceCount = 1;
+  }else{
+    sliceCount = 3;
+  }
   blockItem.slice(0, sliceCount).show();
   btnP.on("click", function(event){
     event.preventDefault();
