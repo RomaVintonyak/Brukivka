9671926e186f7c7aa52c8ccf2f4893242d139865
RomaVintonyak <19roman23@gmail.com>
2020-02-20 12:15:41 +0200

    finished more project script

--- js/block/more__project.js
+++ js/block/more__project.js
@@ -3,11 +3,12 @@ jQuery(document).ready(function(){
   var btnP = $("#PortfolioBtn");
   var blockItem = $(".portfolio__colum");
   blockItem.hide();
-  var sliceCount = 3;
-  if($(window).width() <= 1024){
-    sliceCount = 2;
-  }else if($(window).width() <= 991){
+  var sliceCount = 0;
+  var wWidth = $(window).width();
+  if(wWidth < 768){
     sliceCount = 1;
+  }else if (wWidth < 1024){
+    sliceCount = 2;
   }else{
     sliceCount = 3;
   }
