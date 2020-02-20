c020ff4d9bd144aafd1f9c93b318631c19b327f3
RomaVintonyak <19roman23@gmail.com>
2020-02-20 12:29:55 +0200

    create counter script

---
+++ js/block/counter.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function(){
+  "use script";
+  var counterItem = $(".count__item");
+  var setTime = 30000;
+  counterItem.each(function(){
+    $(this).prop('Counter', 0).animate({
+      Counter: $(this).text()
+    }, {
+      duration: setTime,
+      easing: 'swing',
+      step: function(now){
+        $(this).text(Math.ceil(now));
+      }
+    });
+  });
+});
