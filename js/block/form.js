c30fba863965aa356ed7423b55df9654fa153aa7
RomaVintonyak <19roman23@gmail.com>
2020-02-21 13:48:17 +0200

    create script to send form

--- js/block/form.js
+++ js/block/form.js
@@ -1,44 +1,20 @@
 jQuery(document).ready(function(){
     "use script";
-  $("#formSubmit").on("click", function() {
-    var name = $("#formName")
-      .val()
-      .trim();
-    var mail = $("#formEmail")
-      .val()
-      .trim();
-    var mesag = $("#formMessage")
-      .val()
-      .trim();
-
-    if (name == "") {
-      $("#erorMesag").text("Enter your name");
-      return false;
-    } else if (mail == "") {
-      $("#erorMesag").text("Enter email");
-      return false;
-    } else if (mesag.length < 5) {
-      $("#erorMesag").text("Enter Your Message");
-      return false;
-    }
-    $("#erorMesag").text("");
-
-    $.ajax({
-      url: "../php/form.php",
-      type: "POST",
-      cache: false,
-      data: { name: name, mail: mail, mesag: mesag },
-      dataType: "html",
-      beforeSend: function() {
-        $("#formSubmit").prop("disabled", true);
-      },
-      success: function(data) {
-        if (!data)
-          alert("Message is not published, check all fields correctly");
-        else alert("Thank you for your request");
-        $("#feedback").trigger("reset");
-        $("#formSubmit").prop("disabled", false);
-      }
-    });
-  });
+  
+    $(".form__send").submit(function() {
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "../php/form.php",
+			data: th.serialize()
+		}).done(function() {
+			alert("Дякуємо за Ваше звернення");
+			setTimeout(function() {
+        alert("Повідомлення успішно відправлено");
+        $("#erorMesag").text("");
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
 });
