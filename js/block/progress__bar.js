82f705a022466d26e6dc71ba94b000b693ac4147
RomaVintonyak <19roman23@gmail.com>
2020-02-21 12:40:17 +0200

    create progress bar

---
+++ js/block/progress__bar.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function(){
+  "use script";
+$(function () {
+    $(window).on("scroll resize", function () {
+      var dHeight = $(document).height();
+      var scrolTop = $(window).scrollTop();
+      var wHeight = $(window).height();
+      var prog =
+            scrolTop / (dHeight - wHeight);
+        $("#progress__bar").css({
+            width: ((100 * prog) | 0) + "%"
+        });
+    });
+});
+});
