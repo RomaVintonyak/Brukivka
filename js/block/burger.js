3da5f07261bf36682afa1cbc203f6ad401a9d3e2
RomaVintonyak <19roman23@gmail.com>
2020-02-24 14:06:07 +0200

    create mobile menu

--- js/block/burger.js
+++ js/block/burger.js
@@ -3,5 +3,6 @@ jQuery(document).ready(function(){
   $(".burger__button").on("click", function(event){
     event.preventDefault();
     $(".burger__icon").toggleClass("burger__icon--open");
+    $(".mobile__content").toggleClass("mobile__content--open");
   });
 });
--- js/block/nav__scroll.js
+++ js/block/nav__scroll.js
@@ -4,7 +4,7 @@ jQuery(document).ready(function(){
   navBtn.on("click", function(event){
     event.preventDefault();
     $(".burger__icon").removeClass("burger__icon--open");
-    /*$(".mobile__menu").removeClass("mobile__menu--open");*/
+    $(".mobile__content").removeClass("mobile__content--open");
     var id = $(this).attr("href");
     var offsetTop = $(id).offset().top;
     $("html, body").animate({
